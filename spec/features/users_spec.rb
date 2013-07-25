3cd29a975c6af8b1aae515ca4646f55681bd3383
BM5k <me@bm5k.com>
2013-07-24 17:15:45 -0700

    refactor: improve spec organization

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -4,6 +4,8 @@ describe 'Users' do
 
   subject { page }
 
+  let(:user) { FactoryGirl.create :user }
+
   describe 'sign up' do
 
     before do
@@ -23,6 +25,52 @@ describe 'Users' do
       click_button 'Sign up'
 
       should have_content('A message with a confirmation link has been sent to your email address.')
+    end
+
+  end
+
+  describe 'signing in' do
+
+    it 'is linked to from the home page' do
+      visit root_path
+      should have_css("a[href='#{ new_user_session_path }']")
+    end
+
+    describe 'with email' do
+
+      before do
+        visit new_user_session_path
+      end
+
+      it 'can sign in' do
+        # invalid data
+        click_button 'Sign in'
+        should have_content('Invalid email or password')
+
+        # valid data
+        fill_in_fields :user, email: user.email, password: 'password'
+        click_button 'Sign in'
+        should have_content('Signed in successfully')
+      end
+
+    end
+
+  end
+
+  describe 'signing out' do
+
+    before do
+      login_as user
+    end
+
+    it 'is linked to' do
+      should have_css("a[href='#{ destroy_user_session_path }']")
+    end
+
+    it 'can sign out', :js do
+      click_link user.email
+      find_link('sign out').click
+      should have_content('Signed out successfully')
     end
 
   end
