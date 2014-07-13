2ed528e2ed6a69f2d2ebbcbe14e8771b806c9f7c
BM5k <me@bm5k.com>
2014-07-13 00:26:32 -0500

    remove attr_accessible calls

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,10 +11,6 @@ class User < ActiveRecord::Base
          :trackable,
          :validatable
 
-  # attr_accessible :email, :password, :password_confirmation, :remember_me, as: [:default, :admin]
-  #
-  # attr_accessible :admin, as: [:admin]
-
   scope :access_locked,   ->{ where 'locked_at IS NOT NULL AND locked_at >= ?', unlock_in.ago }
   scope :access_unlocked, ->{ where 'locked_at IS NULL OR locked_at < ?',       unlock_in.ago }
 
