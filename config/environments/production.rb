e5c3e8d9146292588cb6b352f39edd0d21874394
BM5k <me@bm5k.com>
2014-10-23 15:27:13 -0700

    update config stuff to be more inline with rails 4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,18 +1,20 @@
 DevFuBase::Application.configure do
-  config.action_controller.perform_caching = true
-  config.active_support.deprecation        = :notify
-  config.assets.cache_store                = :dalli_store
-  config.assets.compile                    = false
-  config.assets.compress                   = true
-  config.assets.digest                     = true
-  config.cache_classes                     = true
-  config.consider_all_requests_local       = false
-  config.eager_load                        = true
-  config.force_ssl                         = true
-  config.i18n.fallbacks                    = true
-  config.log_tags                          = [ :uuid ]
-  config.serve_static_assets               = true
-  config.static_cache_control              = 'public, max-age=31536000'
+  config.action_controller.perform_caching         = true
+  config.active_record.dump_schema_after_migration = false
+  config.active_support.deprecation                = :notify
+  config.assets.cache_store                        = :dalli_store
+  config.assets.compile                            = false
+  config.assets.compress                           = true
+  config.assets.digest                             = true
+  config.cache_classes                             = true
+  config.consider_all_requests_local               = false
+  config.eager_load                                = true
+  config.force_ssl                                 = true
+  config.i18n.fallbacks                            = true
+  config.log_formatter                             = ::Logger::Formatter.new
+  config.log_tags                                  = [ :uuid ]
+  config.serve_static_assets                       = true
+  config.static_cache_control                      = 'public, max-age=31536000'
 
   config.action_mailer.default_url_options = { host: ENV['EMAIL_HOST'] }
   config.action_mailer.smtp_settings = {
